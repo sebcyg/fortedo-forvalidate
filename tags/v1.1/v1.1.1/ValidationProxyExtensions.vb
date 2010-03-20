6f5a0d826c9445203caaaf31383c07a64d4d3340
sebcyg <sebcyg@27da5b10-4656-49fa-a45a-8ef5402f0de2>
2010-03-20 19:10:19 +0000

    Validator instancing logic moved to ValidationProxy class Simplified SetProxy(Of TValidator) extension

---
+++ tags/v1.1/v1.1.1/ValidationProxyExtensions.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Module ValidationProxyExtensions
+    <Extension()> _
+    Public Function GetProxy(ByVal target As Object) As ValidationProxy
+        Return ValidationProxy.GetProxy(target)
+    End Function
+
+    <Extension()> _
+    Public Function SetProxy(ByVal target As Object, ByVal validator As IValidator) As ValidationProxy
+        Return ValidationProxy.SetProxy(target, validator)
+    End Function
+
+    <Extension()> _
+    Public Function SetProxy(Of TValidator As {IValidator, New})(ByVal target As Object) As ValidationProxy
+        Return ValidationProxy.SetProxy(target, ValidationProxy.GetValidatorInstance(Of TValidator)())
+    End Function
+
+    <Extension()> _
+    Public Sub RemoveProxy(ByVal target As Object)
+        ValidationProxy.RemoveProxy(target)
+    End Sub
+
+    <Extension()> _
+    Public Function ForceValidation(ByVal target As Object) As Boolean
+        Dim proxy = ValidationProxy.GetProxy(target)
+        If proxy Is Nothing Then
+            Return False
+        Else
+            proxy.ForceValidation()
+            Return True
+        End If
+
+    End Function
+End Module
